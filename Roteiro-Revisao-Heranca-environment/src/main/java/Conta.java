eee7f3153dbe5f9d20604fd149191a0dee8d0fac
Nicácio <nicacio.sousa@ccc.ufcg.edu.br>
2017-10-17 16:26:22 -0300

    RR1

---
+++ Roteiro-Revisao-Heranca-environment/src/main/java/Conta.java
@@ -0,0 +1,10 @@
+public class Conta extends ContaAbstrata{
+  
+  public Conta(String numero, double saldo, Cliente cliente) {
+    super(numero, saldo, cliente);
+  }
+
+  public void debitar(double valor) {
+    this.setSaldo(this.getSaldo());
+  }
+}
